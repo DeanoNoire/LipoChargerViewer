d11fcd98791a77668f7dcfe46758240305951acd
deano <rykala.dan@gmail.com>
2022-02-19 00:50:53 +0100

    Beggining

--- app/src/main/java/com/example/lipochargerviewer/MainActivity.kt
+++ app/src/main/java/com/example/lipochargerviewer/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.lipochargerviewer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
+
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ssidString = checkWifiOnAndConnected(this)
+        println(ssidString)
+        ssid_textview.text = ssidString
+
+        val wifiOkVal = vyhodnoceniSSID(ssidString)
+        println(wifiOkVal)
+
+        if(wifiOkVal == 0){
+            enableWifiButton.visibility = View.VISIBLE
+        }
+
+
+    }
+
+
+
+
+    private fun checkWifiOnAndConnected(context: Context): String {
+        val wifiManager = applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
+        val info = wifiManager.connectionInfo
+        val infoUvoz = info.ssid.replace("\"","")
+        return infoUvoz
+    }
+
+    private fun vyhodnoceniSSID(ssidString: String): Int{
+        val res = when (ssidString){
+            "Rykalovi" -> 1
+            "Rykalovi - patro" ->  1
+            else -> 0
+        }
+        return res
     }
 }
---
+++ app/src/main/java/com/example/lipochargerviewer/WifiChecker.kt
@@ -0,0 +1,11 @@
+package com.example.lipochargerviewer
+
+import android.content.Context
+import android.net.wifi.WifiManager
+import androidx.core.content.ContextCompat.getSystemService
+
+
+class WifiChecker {
+
+
+}
