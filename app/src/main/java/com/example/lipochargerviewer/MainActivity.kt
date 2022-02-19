26586860145da679ae41288a0fcd0517ea379a23
deano <rykala.dan@gmail.com>
2022-02-20 00:18:14 +0100

    Almost finished app

--- app/src/main/java/com/example/lipochargerviewer/MainActivity.kt
+++ app/src/main/java/com/example/lipochargerviewer/MainActivity.kt
@@ -7,43 +7,57 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+
+    val MAIN_URL = "http://www.example.com"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val ssidString = checkWifiOnAndConnected(this)
-        println(ssidString)
-        ssid_textview.text = ssidString
+        checkWifiOnAndConnected(this)
 
-        val wifiOkVal = vyhodnoceniSSID(ssidString)
-        println(wifiOkVal)
-
-        if(wifiOkVal == 0){
-            enableWifiButton.visibility = View.VISIBLE
+        enableWifiButton.setOnClickListener {
+            enableWifi(this)
         }
-
 
     }
 
+    private fun enableWifi(context: Context){
+        var wifiManager: WifiManager? = null
+        wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager?
+        wifiManager!!.setWifiEnabled(true)
+        Thread.sleep(5_0000)
+        checkWifiOnAndConnected(context)
+    }
 
-
-
-    private fun checkWifiOnAndConnected(context: Context): String {
+    private fun checkWifiOnAndConnected(context: Context) {
         val wifiManager = applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
         val info = wifiManager.connectionInfo
         val infoUvoz = info.ssid.replace("\"","")
-        return infoUvoz
+        ssid_textview.text = infoUvoz
+        vyhodnoceniSSID(infoUvoz)
     }
 
-    private fun vyhodnoceniSSID(ssidString: String): Int{
+    private fun vyhodnoceniSSID(ssidString: String){
         val res = when (ssidString){
             "Rykalovi" -> 1
             "Rykalovi - patro" ->  1
             else -> 0
         }
-        return res
+        if(res == 0){
+            enableWifiButton.visibility = View.VISIBLE
+            webView.visibility = View.INVISIBLE
+        } else {
+                enableWifiButton.visibility = View.INVISIBLE
+                webView.visibility = View.VISIBLE
+                videoLoad()
+            }
+        }
+
+    private fun videoLoad(){
+        webView.loadUrl(MAIN_URL)
     }
-}
+
+    }
